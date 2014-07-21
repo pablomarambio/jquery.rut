12530d938a56ae9fe5d2a55531ce6284379879c8
Felipe Lavín Z <felipe@yukei.net>
2014-07-21 16:18:36 -0400

    definicion herramientas de desarrollo con grunt

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		uglify: {
+			dist: {
+				sourceMap: true,
+				preserveComments: 'some',
+				files: {
+					"jquery.rut.min.js": "jquery.rut.js"
+				}
+			}
+		},
+		watch: {
+			uglify: {
+				files: ['./jquery.rut.js'],
+				tasks: ['uglify:dist'],
+				options: {
+					nospawn: true
+				}
+			},
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['watch']);
+};
