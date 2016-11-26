1e8166da4093dcc0e6eaca8535cfe7f054348e75
Pablo Marambio <yo@pablomarambio.cl>
2016-11-26 17:06:33 -0300

    Tests para formatRut

--- test/jquery.rut.spec.js
+++ test/jquery.rut.spec.js
@@ -1,4 +1,4 @@
-describe("jquery.rut", function() {
+describe("jquery.rut / jQuery.formatRut", function() {
   var a;
 
   it("should load jQuery", function() {
@@ -8,5 +8,45 @@ describe("jquery.rut", function() {
   it("should load jQuery.rut", function() {
 
     expect(typeof jQuery.formatRut).toBe("function");
+  });
+  it("should format long rut without previous formatting", function() {
+
+    var rut = "145694841";
+    expect(jQuery.formatRut(rut)).toBe("14.569.484-1");
+  });
+  it("should format short rut without previous formatting", function() {
+
+    var rut = "19";
+    expect(jQuery.formatRut(rut)).toBe("1-9");
+  });
+  it("should format long rut with previous formatting", function() {
+
+    var rut = "14.569.484-1";
+    expect(jQuery.formatRut(rut)).toBe("14.569.484-1");
+  });
+  it("should format short rut with previous formatting", function() {
+
+    var rut = "1-9";
+    expect(jQuery.formatRut(rut)).toBe("1-9");
+  });
+  it("should format long rut with bad formatting 1", function() {
+
+    var rut = "14.569.4841";
+    expect(jQuery.formatRut(rut)).toBe("14.569.484-1");
+  });
+  it("should format long rut with bad formatting 2", function() {
+
+    var rut = "14.5694841";
+    expect(jQuery.formatRut(rut)).toBe("14.569.484-1");
+  });
+  it("should short long rut with bad formatting 1", function() {
+
+    var rut = "1.9";
+    expect(jQuery.formatRut(rut)).toBe("1-9");
+  });
+  it("should short long rut with bad formatting 2", function() {
+
+    var rut = "1..9";
+    expect(jQuery.formatRut(rut)).toBe("1-9");
   });
 });
