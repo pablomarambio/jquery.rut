c5fdb3b90cd1078f634640a07a845e50fcbb9d52
Pablo Marambio <yo@pablomarambio.cl>
2016-11-26 17:41:42 -0300

    ;2Csts para validateRut

---
+++ test/jquery.rut.validateRut.spec.js
@@ -0,0 +1,72 @@
+describe("jquery.rut / jQuery.validateRut", function() {
+  it("should validate long rut without formatting", function() {
+    var rut = "145694841";
+    expect(jQuery.validateRut(rut)).toBe(true);
+  });
+  it("should validate short rut without formatting", function() {
+    var rut = "19";
+    expect(jQuery.validateRut(rut)).toBe(true);
+  });
+  it("should not validate invalid long rut without formatting", function() {
+    var rut = "145694842";
+    expect(jQuery.validateRut(rut)).toBe(false);
+  });
+  it("should not validate invalid short rut without formatting", function() {
+    var rut = "17";
+    expect(jQuery.validateRut(rut)).toBe(false);
+  });
+  it("should validate and split long rut without formatting", function() {
+    var rut = "145694841";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(rut).toBe("14569484");
+      expect(dv).toBe("1");
+    });
+  });
+  it("should validate and split long rut with formatting", function() {
+    var rut = "14.569.484-1";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(rut).toBe("14569484");
+      expect(dv).toBe("1");
+    });
+  });
+  it("should validate and split long rut with bad formatting", function() {
+    var rut = "14.5694841";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(rut).toBe("14569484");
+      expect(dv).toBe("1");
+    });
+  });
+  it("should validate and split short rut without formatting", function() {
+    var rut = "19";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(rut).toBe("1");
+      expect(dv).toBe("9");
+    });
+  });
+  it("should validate and split short rut with formatting", function() {
+    var rut = "1-9";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(rut).toBe("1");
+      expect(dv).toBe("9");
+    });
+  });
+  it("should validate and split short rut with bad formatting", function() {
+    var rut = "1.9";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(rut).toBe("1");
+      expect(dv).toBe("9");
+    });
+  });
+  it("should not validate and split invalid short rut", function() {
+    var rut = "1.7";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(1).toBe(0);
+    });
+  });
+  it("should not validate and split invalid long rut", function() {
+    var rut = "145694842";
+    jQuery.validateRut(rut, function(rut, dv) {
+      expect(1).toBe(0);
+    });
+  });
+});
