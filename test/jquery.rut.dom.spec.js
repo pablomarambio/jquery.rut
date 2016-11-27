c8dc9d30b03f0c4965dab5c48206f6729af81fa4
Pablo Marambio <yo@pablomarambio.cl>
2016-11-27 11:19:29 -0300

    Tests de formateo en el evento change

--- test/jquery.rut.dom.spec.js
+++ test/jquery.rut.dom.spec.js
@@ -14,31 +14,79 @@ describe("jquery.rut", function() {
   beforeEach(function() {
     jQuery("body").append(jQuery("<input type='text' id='txtRut' name='txtRut' value='' />"));
     jQuery("body").append(jQuery("<input type='text' id='txt2' name='txt2' value='' />"));
-    $("#txtRut").rut({formatOn: 'keydown'});
   });
 
   afterEach(function() {
     jQuery("#txtRut").remove();
   });
 
-  it ("should format a short rut typed into a textbox", function() {
+  it ("should format a short rut as it is typed into a textbox", function() {
+    $("#txtRut").rut({formatOn: 'keydown'});
     $("#txtRut").focus();
     keyPress(1, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1");
     keyPress(9, $("#txtRut"));
     expect($("#txtRut").val()).toBe("1-9");
   });
 
-  it ("should format a long rut typed into a textbox", function() {
+  it ("should format a long rut as it is typed into a textbox", function() {
+    $("#txtRut").rut({formatOn: 'keydown'});
     $("#txtRut").focus();
     keyPress(1, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1");
     keyPress(5, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1-5");
     keyPress(7, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("15-7");
     keyPress(7, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("157-7");
     keyPress(6, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1.577-6");
     keyPress(8, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("15.776-8");
     keyPress(4, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("157.768-4");
     keyPress(4, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1.577.684-4");
     keyPress(1, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("15.776.844-1");
+  });
+
+  it ("should format a short rut on change", function() {
+    $("#txtRut").rut({formatOn: 'change'});
+    $("#txtRut").focus();
+    keyPress(1, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1");
+    keyPress(9, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("19");
+    $("#txt2").focus();
+    $("#txtRut").change();
+    expect($("#txtRut").val()).toBe("1-9");
+  });
+
+  it ("should format a long rut on change", function() {
+    $("#txtRut").rut({formatOn: 'change'});
+    $("#txtRut").focus();
+    keyPress(1, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1");
+    keyPress(5, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("15");
+    keyPress(7, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("157");
+    keyPress(7, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1577");
+    keyPress(6, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("15776");
+    keyPress(8, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("157768");
+    keyPress(4, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("1577684");
+    keyPress(4, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("15776844");
+    keyPress(1, $("#txtRut"));
+    expect($("#txtRut").val()).toBe("157768441");
+    $("#txt2").focus();
+    $("#txtRut").change();
     expect($("#txtRut").val()).toBe("15.776.844-1");
   });
 });
